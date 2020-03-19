4b5414320c8b138ceb6c59b104be49807fce1943
akiovinicius <50343394+akiovinicius@users.noreply.github.com>
2020-03-18 21:51:33 -0300

    Add files via upload

--- coronavirus.py
+++ coronavirus.py
@@ -6,7 +6,7 @@ df = df.set_index('Country,Other')
 df = df.fillna(0)
 data = list(df.loc['Brazil']) 
 data = list(map(str,data))
-
+print('table ok')
 total_cases = data[0]
 new_cases = data[1]
 total_deaths = data[2]
@@ -14,15 +14,15 @@ new_deaths = data[3]
 active_cases = data[4]
 total_recovered = data[5]
 serious_critical = data[6]
-
-
+print('data ok')
+print('começo do email')
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
 server.ehlo()
 
 server.login('email', 'password')
-
+print('login ok')
 subject = 'Corongavirus no Brasil'
 
 body = 'Hoje no ' + 'Brasil' + '\
@@ -35,12 +35,15 @@ body = 'Hoje no ' + 'Brasil' + '\
     \nCasos criticos: ' + serious_critical  + '\
     \nFontes: https://www.worldometers.info/coronavirus/' + '\
     \nLave as maos!'
-
+print('body ok')
 msg = f"Subject: {subject}\n\n{body}"
+print('assunto ok')
+
+to_emails = ['emails list']
 
 server.sendmail(
     'Coronavirus',
-    'emails',
+    to_emails,
     msg
 )
 print('e-mail enviado')
